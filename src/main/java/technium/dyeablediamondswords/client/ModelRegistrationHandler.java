2f91ef5e6b8a90b3bea438b7cf79dbcbe4238fc4
Technium <techniumunlimited@gmail.com>
2020-07-20 22:10:17 -0700

    Version 1.0
    First Release

---
+++ src/main/java/technium/dyeablediamondswords/client/ModelRegistrationHandler.java
@@ -0,0 +1,40 @@
+package technium.dyeablediamondswords.client;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import technium.dyeablediamondswords.DyeableDiamondSwords;
+import technium.dyeablediamondswords.init.ModItems;
+
+import java.util.Objects;
+
+@EventBusSubscriber(value = Side.CLIENT, modid = DyeableDiamondSwords.MODID)
+public class ModelRegistrationHandler {
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+        registerModel(ModItems.DARK_PURPLE_DIAMOND_SWORD, 0);
+        registerModel(ModItems.DEEP_BLUE_DIAMOND_SWORD, 0);
+        registerModel(ModItems.DEEP_PURPLE_DIAMOND_SWORD, 0);
+        registerModel(ModItems.FIRE_RED_DIAMOND_SWORD, 0);
+        registerModel(ModItems.OCEAN_BLUE_DIAMOND_SWORD, 0);
+        registerModel(ModItems.ORANGE_DIAMOND_SWORD, 0);
+        registerModel(ModItems.PURPLE_DIAMOND_SWORD, 0);
+        registerModel(ModItems.RED_DIAMOND_SWORD, 0);
+        registerModel(ModItems.SKY_BLUE_DIAMOND_SWORD, 0);
+        registerModel(ModItems.YELLOW_DIAMOND_SWORD, 0);
+        registerModel(ModItems.GRAY_DIAMOND_SWORD, 0);
+        registerModel(ModItems.BLACK_DIAMOND_SWORD, 0);
+        registerModel(ModItems.WHITE_DIAMOND_SWORD, 0);
+    }
+
+    private static void registerModel(Item item, int meta) {
+        ModelLoader.setCustomModelResourceLocation(item, meta,
+                new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), "inventory"));
+    }
+
+}
---
+++ src/main/java/technium/dyeablediamondswords/init/ModItems.java
@@ -0,0 +1,25 @@
+package technium.dyeablediamondswords.init;
+
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import technium.dyeablediamondswords.DyeableDiamondSwords;
+
+@ObjectHolder(DyeableDiamondSwords.MODID)
+public class ModItems {
+
+    public static final Item DARK_PURPLE_DIAMOND_SWORD = null;
+    public static final Item DEEP_BLUE_DIAMOND_SWORD = null;
+    public static final Item DEEP_PURPLE_DIAMOND_SWORD = null;
+    public static final Item FIRE_RED_DIAMOND_SWORD = null;
+    public static final Item OCEAN_BLUE_DIAMOND_SWORD = null;
+    public static final Item ORANGE_DIAMOND_SWORD = null;
+    public static final Item PURPLE_DIAMOND_SWORD = null;
+    public static final Item RED_DIAMOND_SWORD = null;
+    public static final Item SKY_BLUE_DIAMOND_SWORD = null;
+    public static final Item YELLOW_DIAMOND_SWORD  = null;
+    public static final Item GRAY_DIAMOND_SWORD = null;
+    public static final Item BLACK_DIAMOND_SWORD = null;
+    public static final Item WHITE_DIAMOND_SWORD = null;
+
+}
